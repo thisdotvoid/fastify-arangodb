dc61b7f1db7170506acfc446e4a6ee39e14bb400
Mirza Brunjadze <brunjadzem@gmail.com>
2018-08-23 16:49:24 +0400

    feat: provide credentials in options

--- index.js
+++ index.js
@@ -3,14 +3,27 @@ const ArangoDB = require('arangojs')
 const isObject = require('lodash.isobject')
 
 function fastifyArangoDB (fastify, options, next) {
-  if (!isObject(options)) {
+  if (!isObject(options) || !isObject(options.config)) {
     next(
       new Error(
         'ArangoDB Database options should be an object https://github.com/arangodb/arangojs#new-database'
       )
     )
   }
-  const arango = new ArangoDB.Database(options)
+
+  const arango = new ArangoDB.Database(options.config)
+
+  if (options.database) {
+    arango.useDatabase(options.database)
+  }
+
+  if (options.basicAuth) {
+    arango.useBasicAuth(
+      options.basicAuth.username,
+      options.basicAuth.password
+    )
+  }
+
   fastify.decorate('arango', arango).addHook("onClose", close)
 
   next()
